55b4b4c502f5c216c8a95a46f140c7bcff972001
netomatiazi <netoomn@gmail.com>
2020-11-27 14:20:09 -0300

    adding the build control component

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,12 @@
+// Funcional Component
+import React from 'react'
+import classes from './BuildControls.css'
+import BuildControl from '../BuildControl/BuildControl'
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        <BuildControl />
+    </div>
+)
+
+export default buildControls
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,14 +5,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIgredient'
 import classes from './Burger.css'
 const burger = (props) => {
     // Mwétodo chave que extrai as chaves de um determinado objeto e transforma isso em uma matriz.
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             // Lida com ingredientes e quantidades dinâmicamente
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 // Retornando o BurgerIngredient
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             })
-        })
+            // Se não houver ingredientes, chega como um array com 4 arrays vazios
+            // Reduzindo todos para um único array vazio, ou qdo houverem elementos mostra a qtdade total de arrays
+        }).reduce((arr, el) => {
+            return arr.concat(el)
+        }, [])
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
@@ -20,6 +27,7 @@ const burger = (props) => {
             <BurgerIngredient type="bread-bottom" />
         </div>
     )
+
 }
 
 export default burger
