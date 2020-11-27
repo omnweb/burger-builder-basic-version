f31774b6e15e6264183ada8a327e05b2687a1725
netomatiazi <netoomn@gmail.com>
2020-11-27 11:35:55 -0300

    Outputting Burger Ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,16 +4,20 @@ import React from 'react'
 import BurgerIngredient from './BurgerIngredient/BurgerIgredient'
 import classes from './Burger.css'
 const burger = (props) => {
+    // Mwétodo chave que extrai as chaves de um determinado objeto e transforma isso em uma matriz.
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            // Lida com ingredientes e quantidades dinâmicamente
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                // Retornando o BurgerIngredient
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            })
+        })
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="salad" />
-            <BurgerIngredient type="bacon" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
-
-
         </div>
     )
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,24 @@ import Aux from '../../hoc/Auxiliary'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props)
+    //     // Usando o this, pois receberá um método
+    //     this.state = {...}
+    // }
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2,
+        }
+    }
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
         )
