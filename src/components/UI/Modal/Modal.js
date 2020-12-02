7495d23223163ce87adcef28476d3a6209bb7e9f
netomatiazi <netoomn@gmail.com>
2020-12-02 11:38:07 -0300

    Improving performance by avoiding unnecessary reloads

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './Modal.css'
 import Aux from '../../../hoc/Auxiliary'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}
-        >
-            {props.children}
-        </div>
-    </Aux>
-)
+class Modal extends Component {
+    // O modal recarregava cada vez que o controle do construtor era acionado
+    shouldComponentUpdate(nextProps, nextState) {
+        //Só vai atualizar quando for efetivar as mudanças 
+        return nextProps.show !== this.props.show;
+    }
+    // componentDidUpdate() {
+    //     console.log('[Modal] componentDidUpdate')
+    // }
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
 
-
-export default modal
+export default Modal
