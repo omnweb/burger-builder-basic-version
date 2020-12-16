8643f85225d630475d25fd29f524874e0e757d87
netomatiazi <netoomn@gmail.com>
2020-12-16 16:55:39 -0300

    Creating a Custom Dynamic Input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.css'
 import axios from '../../../axio-orders'
 import Spinner from '../../../components/UI/Spinner/Spinner'
+import Input from '../../../components/UI/Input/Input'
 
 
 class ContactData extends Component {
@@ -48,10 +49,10 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-                <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
-                <input className={classes.Input} type="text" name="street" placeholder="Your Street" />
-                <input className={classes.Input} type="text" name="postalCode" placeholder="Your Postal Code" />
+                <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
+                <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
+                <Input inputtype="input" type="text" name="street" placeholder="Your Street" />
+                <Input inputtype="input" type="text" name="postalCode" placeholder="Your Postal Code" />
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER NOW</Button>
                 {/* <Button btnType="Danger" clicked>CANCEL</Button> */}
             </form>
