fc7890df51545716f93041d663fde2e3170a4546
netomatiazi <netoomn@gmail.com>
2021-02-16 09:03:58 -0300

    Finalizing the basic implementation of redux

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,6 @@ class BurgerBuilder extends Component {
     // }
 
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -38,7 +37,7 @@ class BurgerBuilder extends Component {
         //     })
     }
 
-    updetePurchaseState(ingredients) {
+    updatePurchaseState(ingredients) {
         const sum = Object.keys(ingredients) //Cria um array de strings
             .map(igkey => {
                 return ingredients[igkey]
@@ -46,41 +45,9 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el
             }, 0)
-        this.setState({ purchasable: sum > 0 })
+        // this.setState({ purchasable: sum > 0 })
+        return sum > 0
     }
-
-    // addIngredientHandler = (type) => {
-    //     // É Necessário saber o que é o ingrediente antigo.
-    //     const oldCount = this.state.ingredients[type]
-    //     const updatedCount = oldCount + 1
-    //     const updatedIngredients = {
-    //         ...this.state.ingredients
-    //     }
-    //     updatedIngredients[type] = updatedCount
-    //     const priceAddition = INGREDIENT_PRICES[type]
-    //     const oldPrice = this.state.totalPrice
-    //     const newPrice = oldPrice + priceAddition
-
-    //     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
-    //     this.updetePurchaseState(updatedIngredients)
-    // }
-    // removeIngredientHandler = (type) => {
-    //     const oldCount = this.state.ingredients[type]
-    //     if (oldCount <= 0) {
-    //         return // Caso não tenha ingrediente para decrementar ele não executa a retirada.
-    //     }
-    //     const updatedCount = oldCount - 1
-    //     const updatedIngredients = {
-    //         ...this.state.ingredients
-    //     }
-    //     updatedIngredients[type] = updatedCount
-    //     const priceDeduction = INGREDIENT_PRICES[type]
-    //     const oldPrice = this.state.totalPrice
-    //     const newPrice = oldPrice - priceDeduction
-
-    //     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
-    //     this.updetePurchaseState(updatedIngredients)
-    // }
 
     purchaseHandler = () => {
         this.setState({ purchasing: true })
@@ -91,16 +58,7 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        const queryParams = []
-        for (let i in this.state.ingredients) {
-            queryParams.push(encodeURIComponent(i) + '=' + this.state.ingredients[i])
-        }
-        queryParams.push('price=' + this.state.totalPrice)
-        const queryString = queryParams.join('&')
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryString
-        })
+        this.props.history.push('/checkout')
     }
 
     render() {
@@ -121,7 +79,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
                         price={this.props.price}
                     />
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -5,6 +5,7 @@ import axios from '../../../axio-orders'
 import Spinner from '../../../components/UI/Spinner/Spinner'
 import Input from '../../../components/UI/Input/Input'
 
+import { connect } from 'react-redux'
 
 class ContactData extends Component {
     state = {
@@ -104,7 +105,7 @@ class ContactData extends Component {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value //passando o valor do form
         }
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData
         }
@@ -195,4 +196,11 @@ class ContactData extends Component {
 
 }
 
-export default ContactData
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+}
+
+export default connect(mapStateToProps)(ContactData)
