9049280266786de726ec10c762885bf3f8d24135
netomatiazi <netoomn@gmail.com>
2021-02-17 15:13:15 -0300

    Fetching Ingredients Asynchronously

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,5 +1,7 @@
 //1- import actionTypes
 import * as actionTypes from './actionTypes'
+import axios from '../../axio-orders'
+
 
 // Export addIngredient in camelcase format
 export const addIngredient = name => {
@@ -12,5 +14,30 @@ export const removeIngredient = name => {
     return {
         type: actionTypes.REMOVE_INGREDIENT,
         ingredientName: name
+    }
+}
+
+export const setIngredients = (ingredients) => {
+    return {
+        type: actionTypes.SET_INGREDIENTS,
+        ingredients: ingredients
+    }
+}
+
+export const fetchIngredientsFailed = () => {
+    return {
+        type: actionTypes.FETCH_INGREDIENTS_FAILED
+    }
+}
+
+export const initIngredient = () => {
+    return (dispatch) => {
+        axios.get('https://react-my-burger-1e190-default-rtdb.firebaseio.com/ingredients.json')
+            .then(response => {
+                dispatch(setIngredients(response.data))
+            })
+            .catch(error => {
+                dispatch(fetchIngredientsFailed())
+            })
     }
 }
