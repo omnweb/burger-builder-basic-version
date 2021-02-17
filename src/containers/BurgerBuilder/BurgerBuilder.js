4c8c79b1a3879b2163161533e6451761d4b4ed6e
netomatiazi <netoomn@gmail.com>
2021-02-17 14:41:52 -0300

    Executing Asynchronous Code

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,13 +28,13 @@ class BurgerBuilder extends Component {
 
     componentDidMount() {
         // console.log(this.props)
-        // axios.get('https://react-my-burger-1e190-default-rtdb.firebaseio.com/ingredients.json')
-        //     .then(response => {
-        //         this.setState({ ingredients: response.data })
-        //     })
-        //     .catch(error => {
-        //         this.setState({ error: true })
-        //     })
+        axios.get('https://react-my-burger-1e190-default-rtdb.firebaseio.com/ingredients.json')
+            .then(response => {
+                this.setState({ ingredients: response.data })
+            })
+            .catch(error => {
+                this.setState({ error: true })
+            })
     }
 
     updatePurchaseState(ingredients) {
