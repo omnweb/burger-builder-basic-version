a86f359606d9358ab9e433a95224f8f134435ee6
netomatiazi <netoomn@gmail.com>
2021-02-18 11:43:48 -0300

    Combining Reducers

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -88,7 +88,7 @@ class ContactData extends Component {
                     ]
                 },
                 validation: {},
-                value: '',
+                value: 'fastest',
                 valid: true
 
             }
@@ -192,9 +192,9 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients,
-        price: state.totalPrice,
-        loading: state.loading
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        loading: state.order.loading
     }
 }
 
