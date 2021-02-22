3520377ae578db23ecf592eb5434b844bdfbbc33
netomatiazi <netoomn@gmail.com>
2021-02-22 16:43:15 -0300

    Create Authentication component

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,6 +6,7 @@ const NavigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
         <NavigationItem link="/orders">Order</NavigationItem>
+        <NavigationItem link="/auth">Login</NavigationItem>
     </ul>
 )
 export default NavigationItems
