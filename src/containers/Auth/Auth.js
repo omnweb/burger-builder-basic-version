738477ce6f1db43fd87c84c08e2040385027768b
netomatiazi <netoomn@gmail.com>
2021-03-11 16:37:57 -0300

    Redirecting the User to the Checkout Page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -42,6 +42,11 @@ class Auth extends Component {
         isSignUp: true
     }
 
+    componentDidMount() {
+        if (!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
+            this.props.onSetAuthRedirectPath()
+        }
+    }
     checkValidity(value, rules) {
         let isValid = true;
         if (!rules) {
@@ -138,7 +143,7 @@ class Auth extends Component {
 
         let authRedirect = null
         if (this.props.isAutenticated) {
-            authRedirect = <Redirect to="/" />
+            authRedirect = <Redirect to={this.props.authRedirectPath} />
         }
 
         return (
@@ -161,13 +166,16 @@ const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
         error: state.auth.error,
-        isAutenticated: state.auth.token !== null
+        isAutenticated: state.auth.token !== null,
+        buildingBurger: state.burgerBuilder.building,
+        authRedirectPath: state.auth.authRedirectPath
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
+        onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp)),
+        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
     };
 };
 
