59c15ced1b117a5691bfaea9de3ff298321db3e6
netomatiazi <netoomn@gmail.com>
2021-03-12 15:06:31 -0300

    Guarding Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -14,19 +14,38 @@ class App extends Component {
     this.props.onTryAutoSignup()
   }
   render() {
+    let routes = (
+      < Switch >
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch >
+    )
+
+    if (this.props.isAutenticated) {
+      routes = (
+        < Switch >
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch >
+      )
+    }
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          {routes}
         </Layout>
-      </div>
+      </div >
     );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isAutenticated: state.auth.token !== null
   }
 }
 
@@ -36,4 +55,5 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
