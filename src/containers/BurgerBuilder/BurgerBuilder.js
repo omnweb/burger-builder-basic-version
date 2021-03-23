538ea79e5bfb45de94d5d3d4f2df41f1d943856a
netomatiazi <netoomn@gmail.com>
2021-03-23 10:09:28 -0300

    Removing console.log()s

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,7 +25,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
-        console.log(this.props);
+        // console.log(this.props);
         this.props.onInitIngredients();
     }
 
