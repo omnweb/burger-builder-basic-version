7c73c067a90faff84dfec4e9335d51a5573be7da
netomatiazi <netoomn@gmail.com>
2021-03-23 15:10:05 -0300

    Jest and Enzyme Documentations

--- src/components/Navigation/NavigationItems/Navigation.test.js
+++ src/components/Navigation/NavigationItems/Navigation.test.js
@@ -22,4 +22,9 @@ describe('<NavigationItems />', () => {
         wrapper.setProps({ isAutenticated: true })
         expect(wrapper.find(NavigationItem)).toHaveLength(3)
     })
+
+    it('should render tree <NavigationItems /> if authenticated', () => {
+        wrapper.setProps({ isAutenticated: true })
+        expect(wrapper.contains(<NavigationItem link='/logout' >Logout</NavigationItem>)).toEqual(true)
+    })
 })
