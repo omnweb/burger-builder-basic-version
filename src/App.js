69fce6277dddcac8327b46b5ee767da451d4bab3
netomatiazi <netoomn@gmail.com>
2021-03-23 10:42:29 -0300

    Adding Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux'
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
+import './containers/Checkout/Checkout';
+import './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout'
-import { connect } from 'react-redux'
 import * as actions from './store/actions/index'
+
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout')
+})
+const asyncOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders')
+})
+const asyncAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth')
+})
 
 class App extends Component {
   componentDidMount() {
@@ -25,10 +37,10 @@ class App extends Component {
     if (this.props.isAutenticated) {
       routes = (
         < Switch >
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
+          <Route path="/checkout" component={asyncCheckout} />
+          <Route path="/orders" component={asyncOrders} />
           <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={Auth} />
+          <Route path="/auth" component={asyncAuth} />
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to="/" />
         </Switch >
